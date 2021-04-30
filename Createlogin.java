25b3ff31c9c938989e2d5adb93864c8d5f9fc124
noushan1 <60235616+noushan1@users.noreply.github.com>
2021-04-30 12:04:07 +0100

    Update Createlogin.java

--- Createlogin.java
+++ Createlogin.java
@@ -1,197 +1,129 @@
-package automationpractice.com;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+	
+	package com.test.nishmidha;
 
-import automationpractice.com.pageObject.Account;
-import automationpractice.com.pageObject.CreateAccount;
-import automationpractice.com.pageObject.CreateAccountForm;
-import automationpractice.com.pageObject.Homepage;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	import org.testng.Assert;
+	import org.testng.annotations.AfterClass;
+	import org.testng.annotations.BeforeClass;
+	import org.testng.annotations.Test;
+
+import WebElement.WebElement.By;
 import automationpractice.com.pageObject.SignInForm;
-import utils.EmailsGenerator;
+import utils.WebDriver;
 
-public class CreateAccountFormTest {
+	public class CreateAccountFormTest {
 
-	private WebDriver driver;
+		private ChromeDriver driver;
 
-	private Homepage homepage;
-	private CreateAccount createAccount;
-	private CreateAccountForm createAccountForm;
-	private SignInForm signin;
-	private Account account;
+		private Homepage homepage;
+		private CreateAccount createAccount;
+		private CreateAccountForm createAccountForm;
+		private SignInForm signin;
+		private Account account;
 
-	@BeforeClass
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-		driver = new ChromeDriver();
+		public void setup() {
+			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+			driver = new ChromeDriver();
 
-		homepage = new Homepage(driver);
-		createAccount = new CreateAccount(driver);
-		createAccountForm = new CreateAccountForm(driver);
-		signin = new SignInForm(driver);
-		account = new Account(driver);
+			homepage = new Homepage();
+			createAccount = new CreateAccount();
+			createAccountForm = new CreateAccountForm();
+			signin = new SignInForm();
+			account = new Account();
 
-		String baseUrl = "http://automationpractice.com/index.php";
-		driver.manage().window().maximize();
-		driver.get(baseUrl);
-	}
+			String baseUrl = "http://automationpractice.com/index.php";
+			driver.get(baseUrl);
+			
+		}
 
-	@AfterClass
-	public void closeAll() {
-		account.getAccountLogout().click();
-		driver.quit();
-	}
+		public void closeAll() throws InterruptedException {
+			
+			account.getAccountLogout();
+		
+			driver.wait();
+		
+		}
+		
+		@Test
+		public void personalInfoFields() {
+			// With values
+			createAccountForm.setCustomerFirstNameField("Nishi");
+			createAccountForm.setCustomerLastNameField("Naush");
+			createAccountForm.setCustomerEmailField("nishmithaovr@gmail.com");
+			createAccountForm.setCustomerPasswordField("tester");
 
-	@Test(priority = 1)
-	public void authenticationPage() {
-		homepage.getSignInBtn().click();
+			createAccountForm.getAccountCreationForm().click();
 
-		Assert.assertTrue(createAccount.getCreateAccountForm().isDisplayed());
-		Assert.assertTrue(createAccount.getCreatAccountEmailField().isDisplayed());
-		Assert.assertTrue(createAccount.getCreateAccountBtn().isDisplayed());
-		Assert.assertTrue(signin.getSignInForm().isDisplayed());
-	}
+			Assert.assertTrue(createAccountForm.getFirstNameHighlightedGreen().isDisplayed());
+			Assert.assertTrue(createAccountForm.getLastNameHighlightedGreen().isDisplayed());
+			Assert.assertTrue(createAccountForm.getEmailHighlightedGreen().isDisplayed());
+			Assert.assertTrue(createAccountForm.getPasswordHighlightedGreen().isDisplayed());
 
-	@Test(priority = 2)
-	public void authenticationPageEmailField() {
-		// Without email
-		createAccount.getCreateAccountBtn().click();
+			// Without values
+			createAccountForm.setCustomerFirstNameField("");
+			createAccountForm.setCustomerLastNameField("");
+			createAccountForm.setCustomerEmailField("");
+			createAccountForm.setCustomerPasswordField("");
 
-		Assert.assertTrue(createAccount.getEmailErrorMessage().isDisplayed());
+			createAccountForm.getAccountCreationForm().click();
 
-		// Wrong email format (mapko89ct, mapko89ct@gmail ...)
-		createAccount.setCreateAccountEmailField("marko");
-		createAccount.getCreateAccountBtn().click();
+			Assert.assertTrue(createAccountForm.getFirstNameHighlightedRed().isDisplayed());
+			Assert.assertTrue(createAccountForm.getLastNameHighlightedRed().isDisplayed());
+			Assert.assertTrue(createAccountForm.getEmailHighlightedRed().isDisplayed());
+			Assert.assertTrue(createAccountForm.getPasswordHighlightedRed().isDisplayed());
+		}
 
-		Assert.assertTrue(createAccount.getEmailErrorMessage().isDisplayed());
-		Assert.assertTrue(createAccount.getEmailFieldHighlightedRed().isDisplayed());
+		@Test
+		public void requiredFieldsEmpty() {
+			createAccountForm.getAddressAliasField().clear();
+			createAccountForm.setCustomerEmailField("");
+			createAccountForm.selectCountry("-");
+			createAccountForm.getRegisterBtn().click();
 
-		// Registered email
-		createAccount.setCreateAccountEmailField("email@email.email");
-		createAccount.getCreateAccountBtn().click();
+			Assert.assertTrue(createAccountForm.getPhoneNumberError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getLastNameError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getFirstNameError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getEmailRequiredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getPasswordRequiredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getCountryRequiredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getAddressRequiredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getAddressAliasRequiredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getCityRequiredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getCountryUnselectedError().isDisplayed());
 
-		Assert.assertTrue(createAccount.getEmailBeenRegistered().isDisplayed());
+			createAccountForm.selectCountry("United States");
+			createAccountForm.getRegisterBtn().click();
 
-		// Correct email
-		createAccount.setCreateAccountEmailField("mapko123ct@gmail.com");
-		createAccount.getCreateAccountBtn().click();
+			Assert.assertTrue(createAccountForm.getStateRequredError().isDisplayed());
+			Assert.assertTrue(createAccountForm.getPostalCodeError().isDisplayed());
+		}
 
-		Assert.assertTrue(createAccountForm.getAccountCreationForm().isDisplayed());
-	}
+		
+		@Test
+		public void createAccountSuccessfully() {
+			// Required fields filled
+			createAccountForm.setCustomerFirstNameField("Test1");
+			createAccountForm.setCustomerLastNameField("Test2");
+			createAccountForm.setCustomerPasswordField("test23");
+			createAccountForm.selectCustomerDateOfBirthDay("15");
+			createAccountForm.selectCustomerDateOfBirthMonth("10");
+			createAccountForm.selectCustomerDateOfBirthYear("1988");
+			createAccountForm.setAddressField("Cottages");
+			createAccountForm.setCityField("Hilling street");
+			createAccountForm.selectState("78");
+			createAccountForm.setPostalCodeField("98567");
+			createAccountForm.setHomePhoneField("07");
+			createAccountForm.setMobilePhoneField("077");
+			createAccountForm.setAddressAliasField("Test address");
+			createAccountForm.getRegisterBtn().click();
 
-	@Test(priority = 3)
-	public void personalInfoFields() {
-		// With values
-		createAccountForm.setCustomerFirstNameField("Marko");
-		createAccountForm.setCustomerLastNameField("Stevanovic");
-		createAccountForm.setCustomerEmailField("mapko89ct@gmail.com");
-		createAccountForm.setCustomerPasswordField("tester");
+			Assert.assertTrue(createAccountForm.getEmailBeenRegistered().isDisplayed());
 
-		createAccountForm.getAccountCreationForm().click();
+			createAccountForm.setCustomerEmailField(EmailsGenerator.getNextEmail());
+			createAccountForm.setCustomerPasswordField("tester123");
+			createAccountForm.getRegisterBtn().click();
 
-		Assert.assertTrue(createAccountForm.getFirstNameHighlightedGreen().isDisplayed());
-		Assert.assertTrue(createAccountForm.getLastNameHighlightedGreen().isDisplayed());
-		Assert.assertTrue(createAccountForm.getEmailHighlightedGreen().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPasswordHighlightedGreen().isDisplayed());
-
-		// Without values
-		createAccountForm.setCustomerFirstNameField("");
-		createAccountForm.setCustomerLastNameField("");
-		createAccountForm.setCustomerEmailField("");
-		createAccountForm.setCustomerPasswordField("");
-
-		createAccountForm.getAccountCreationForm().click();
-
-		Assert.assertTrue(createAccountForm.getFirstNameHighlightedRed().isDisplayed());
-		Assert.assertTrue(createAccountForm.getLastNameHighlightedRed().isDisplayed());
-		Assert.assertTrue(createAccountForm.getEmailHighlightedRed().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPasswordHighlightedRed().isDisplayed());
-	}
-
-	@Test(priority = 4)
-	public void requiredFieldsEmpty() {
-		createAccountForm.getAddressAliasField().clear();
-		createAccountForm.setCustomerEmailField("");
-		createAccountForm.selectCountry("-");
-		createAccountForm.getRegisterBtn().click();
-
-		Assert.assertTrue(createAccountForm.getPhoneNumberError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getLastNameError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getFirstNameError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getEmailRequiredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPasswordRequiredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getCountryRequiredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getAddressRequiredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getAddressAliasRequiredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getCityRequiredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getCountryUnselectedError().isDisplayed());
-
-		createAccountForm.selectCountry("United States");
-		createAccountForm.getRegisterBtn().click();
-
-		Assert.assertTrue(createAccountForm.getStateRequredError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPostalCodeError().isDisplayed());
-	}
-
-	@Test(priority = 5)
-	public void requiredFieldsInputFormat() throws Exception {
-		// Wrong format
-		createAccountForm.setCustomerEmailField("mapko89ct@gmail");
-		createAccountForm.setCustomerPasswordField("test");
-		createAccountForm.setPostalCodeField("asd");
-		createAccountForm.setHomePhoneField("asd");
-		createAccountForm.setMobilePhoneField("asd");
-
-		createAccountForm.getRegisterBtn().click();
-
-		Assert.assertTrue(createAccountForm.getEmailInvalidError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPasswordInvalidError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPostalCodeError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getHomePhoneInvalidError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getMobilePhoneInvalidError().isDisplayed());
-
-		// Correct format
-		createAccountForm.setCustomerEmailField("email1@gmail.com");
-		createAccountForm.setCustomerPasswordField("tester");
-		createAccountForm.setPostalCodeField("21000");
-		createAccountForm.setHomePhoneField("056");
-		createAccountForm.setMobilePhoneField("065");
-
-		Assert.assertTrue(createAccountForm.getEmailInvalidError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPasswordInvalidError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getPostalCodeError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getHomePhoneInvalidError().isDisplayed());
-		Assert.assertTrue(createAccountForm.getMobilePhoneInvalidError().isDisplayed());
-	}
-
-	@Test(priority = 6)
-	public void createAccountSuccessfully() {
-		// Required fields filled
-		createAccountForm.setCustomerFirstNameField("John");
-		createAccountForm.setCustomerLastNameField("Doe");
-		createAccountForm.setCustomerPasswordField("tester123");
-		createAccountForm.selectCustomerDateOfBirthDay("20");
-		createAccountForm.selectCustomerDateOfBirthMonth("10");
-		createAccountForm.selectCustomerDateOfBirthYear("2000");
-		createAccountForm.setAddressField("Centar");
-		createAccountForm.setCityField("Novi Sad");
-		createAccountForm.selectState("7");
-		createAccountForm.setPostalCodeField("21000");
-		createAccountForm.setHomePhoneField("056");
-		createAccountForm.setMobilePhoneField("066");
-		createAccountForm.setAddressAliasField("My Address");
-		createAccountForm.getRegisterBtn().click();
-
-		Assert.assertTrue(createAccountForm.getEmailBeenRegistered().isDisplayed());
-
-		createAccountForm.setCustomerEmailField(EmailsGenerator.getNextEmail());
-		createAccountForm.setCustomerPasswordField("tester123");
-		createAccountForm.getRegisterBtn().click();
-
-		Assert.assertTrue(createAccountForm.successfullyCreatedAccount().isDisplayed());
-	}
-}
+			Assert.assertTrue(createAccountForm.successfullyCreatedAccount().isDisplayed());
+		}
